5d7486ba0c1eda9be4da0ad41126da1111f352b6
Chris Malin <csmalin@ubuntu.(none)>
2013-04-05 14:22:37 -0700

    I think the migration is working...

--- db/migrate/20130405124541_create_tables.rb
+++ db/migrate/20130405124541_create_tables.rb
@@ -2,32 +2,31 @@ class CreateTables < ActiveRecord::Migration
   def change
 
   	create_table(:users) do |user|
-  		user.string :email, :unique => true
-  		user.string :password
+  		user.string :email, :unique => true, null: false
+  		user.string :password_hash
   		user.timestamps
   	end	
 
   	create_table(:decks) do |deck|
-  		deck.string :name
+  		deck.string :name, null: false
   		deck.timestamps
   	end	
 
   	create_table(:cards) do |card|
-  		card.string :term 
-  		card.text :definition
+  		card.string :term, null: false
+  		card.string :definition, null: false
   		card.timestamps
-  		card.references :decks
+  		card.references :deck
   	end
 
   	create_table(:rounds) do |round|
-  		round.integer :number
-  		round.references :users, :decks
+  		round.references :user, :deck
   		round.timestamps
   	end
 
   	create_table(:guesses) do |guess|
-  		guess.boolean :correct
-  		guess.references :cards, :rounds
+  		guess.boolean :correct, null: true
+  		guess.references :card, :round
     end
 
   end
