23e4c18e319c23a9c1c44cd1c23492821954cb4d
Chris Malin <csmalin@ubuntu.(none)>
2013-04-05 16:17:33 -0700

    auth, login, logout, create account

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,36 @@
+before do
+  @current_user = User.find_by_token(session[:token])
+end
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
+end
+
+post '/login' do
+  @user = User.authenticate(params[:email].downcase, params[:password])
+  if @user
+    @user.login
+    session[:token] = @user.token
+    redirect '/game'
+  else
+    redirect '/'
+  end
+end
+
+get '/logout' do
+  session.destroy
+  redirect '/'
+end
+
+
+post '/signup' do 
+  user = User.create(params)
+  user.login
+  session[:token] = user.token
+  redirect '/game'
+end
+
+get '/game' do
+"Welcome to the game page!!!"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
 class User < ActiveRecord::Base
   has_many :rounds
+
+	include BCrypt
+
+	def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password)
+    @user = User.find_by_email(email)
+    return @user if (@user.password == password) 
+    nil
+  end
+
+  def login
+    self.update_attributes(:token => SecureRandom.uuid)
+  end
+
 end
