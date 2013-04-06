0acbb1ff51b860275e2e60bfb41f77b97835f272
AlexRay <alex.james.ray@gmail.com>
2013-04-05 17:36:13 -0700

    game logic refactor

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -1,30 +1,36 @@
-get "/game/:deck_id" do
- current_user = User.find_by_token(session[:token])#find_by_email("ben@email.com")
- round = current_user.rounds << Round.create()
- deck = round.last.deck = Deck.find(params[:deck_id])
- cards = deck.cards
- card_ids = []
+before do 
+ @current_user = User.find_by_token(session[:token])#find_by_email("ben@email.com")
+ @round = @current_user.rounds << Round.create()
+ @deck = @round.last.deck = Deck.find(params[:deck_id])
+ @cards = @deck.cards
+end
 
-  cards.each do |card|
+get "/game/:deck_id" do
+  
+  card_ids = []
+  @cards.each do |card|
     card_ids << card.id 
   end
 
   session[:cards] = card_ids
 
   @card = Card.find(session[:cards].shift)
-  puts @card.inspect
+  @deck
+
   erb :game
 end
 
 post "/game/guess/:card_id" do 
   guess = params[:answer]
-  card = Card.find(params[:card_id])
 
-  is_correct = (card.definition == guess) 
+  previous_card = Card.find(params[:card_id])
+
+  is_correct = (previous_card.definition == guess) 
 
   card.guesses << Guess.create(correct: is_correct)
 
   @card = Card.find(session[:cards].shift)
+  @deck
 
   erb :game
 end
